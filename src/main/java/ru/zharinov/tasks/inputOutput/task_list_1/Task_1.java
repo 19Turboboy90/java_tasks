7a44fcd720adb5a272d87ebbb87e6f9ad76e4a38
Ilia Zharinov <19turboboy90@gmail.com>
2023-08-12 20:44:13 +0300

    Added new package "Output - Input" and Task_1

---
+++ src/main/java/ru/zharinov/tasks/inputOutput/task_list_1/Task_1.java
@@ -0,0 +1,29 @@
+package ru.zharinov.tasks.inputOutput.task_list_1;
+
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/*
+Задан файл с текстом, найти и вывести в консоль все слова, начинающиеся с гласных букв.
+ */
+public class Task_1 {
+    private static final String VOWELS = "уеоаыяию";
+
+    public static void main(String[] args) {
+
+        Path path = Path.of("C:\\Users\\Family\\Desktop\\test.txt");
+        try (Scanner scanner = new Scanner(path)) {
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                char firstSymbol = word.charAt(0);
+                if (VOWELS.indexOf(firstSymbol) != -1){
+                    System.out.println(word);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
