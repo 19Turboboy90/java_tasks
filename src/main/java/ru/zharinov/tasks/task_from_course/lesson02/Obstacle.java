ea749c7305daf76b8f0b93c1f539dc7a1aac7ef2
Ilia Zharinov <19Turboboy90@gmail.com>
2024-02-13 21:00:57 +0300

    Add new task

---
+++ src/main/java/ru/zharinov/tasks/task_from_course/lesson02/Obstacle.java
@@ -0,0 +1,5 @@
+package ru.zharinov.tasks.task_from_course.lesson02;
+
+public abstract class Obstacle {
+    public abstract boolean action(Contestant contestant);
+}
---
+++ src/main/java/ru/zharinov/tasks/task_from_course/lesson02/Wall.java
@@ -0,0 +1,20 @@
+package ru.zharinov.tasks.task_from_course.lesson02;
+
+public class Wall extends Obstacle{
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean action(Contestant contestant) {
+        if (contestant.jump(height)) {
+            System.out.println("Прыжок успешен у " + contestant.toString());
+            return true;
+        } else {
+            System.out.println("Прыжок не получился у " + contestant.toString());
+            return false;
+        }
+    }
+}
