2ab1368afeb8df1c2c11c19eed3541bddf451d99
Ilia Zharinov <19Turboboy90@gmail.com>
2024-02-15 15:52:41 +0300

    Add new homework

---
+++ src/main/java/ru/zharinov/tasks/task_from_course/lesson03/exceptions/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.zharinov.tasks.task_from_course.lesson03.exceptions;
+
+public class MyArraySizeException extends MyException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/zharinov/tasks/task_from_course/lesson03/exceptions/MyException.java
@@ -0,0 +1,7 @@
+package ru.zharinov.tasks.task_from_course.lesson03.exceptions;
+
+public class MyException extends RuntimeException {
+    public MyException(String message) {
+        super(message);
+    }
+}
