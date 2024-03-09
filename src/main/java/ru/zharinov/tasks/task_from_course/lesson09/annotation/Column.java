787ed0c5a1d0743dbd2782105c7450fd6c3a8e0e
Ilia Zharinov <19Turboboy90@gmail.com>
2024-03-09 22:50:09 +0300

    Add new homework

---
+++ src/main/java/ru/zharinov/tasks/task_from_course/lesson09/annotation/Column.java
@@ -0,0 +1,11 @@
+package ru.zharinov.tasks.task_from_course.lesson09.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+}
